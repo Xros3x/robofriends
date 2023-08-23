bf4aa74626ca08d45bde79e573bcd42b6f770e4d
Tyrik <100667720+Xros3x@users.noreply.github.com>
2023-08-23 08:35:39 -0500

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import { robots } from "../robots";
+import "./App.css";
+import Scroll from "../components/Scroll";
+import ErrorBoundry from "../components/ErrorBoundry";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ robots: robots });
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/robots.js
@@ -0,0 +1,86 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Bolt Steel',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+    },
+    {
+      id: 2,
+      name: 'Circuit Techton',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+    },
+    {
+      id: 3,
+      name: 'Nova Gearhart',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net'
+    },
+    {
+      id: 4,
+      name: 'Pulse Voltage',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 5,
+      name: 'Orion Ironsmith',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 6,
+      name: 'Electra Binary',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 7,
+      name: 'Spark Robinsen',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 8,
+      name: 'Titan Watts',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 9,
+      name: 'Athena Titanium',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 10,
+      name: 'Nexus Copperfield',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    },
+    {
+      id: 11,
+      name: 'Quasar Quantum',
+      username: 'Cruise',
+      email: 'Crops@lap.biz'
+    },
+    {
+      id: 12,
+      name: 'Aurora Synthson',
+      username: 'Apili',
+      email: 'Apili@list.tv'
+    },
+    {
+      id: 13,
+      name: 'Zenith Algorithm',
+      username: 'Kayle',
+      email: 'Kayle@lee.net'
+    },
+    {
+      id: 14,
+      name: 'Sirius Mechanix',
+      username: 'Kara',
+      email: 'Kara@click.org'
+    },
+  ];
